934a82ef89e78ea6ae082bccd25b6cee78cfbf1a
Nathan Elmer <nelmer12@gmail.com>
2022-03-02 10:16:06 -0600

    robe.cs

--- Adventurer.cs
+++ Adventurer.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Quest
 {
     // An instance of the Adventurer class is an object that will undergo some challenges
@@ -11,12 +13,19 @@ namespace Quest
         // This is a mutable property it has a "get" and a "set"
         //  So it can be read and changed by any code in the application
         public int Awesomeness { get; set; }
+        public Robe ColorfulRobe { get; }
 
         // A constructor to make a new Adventurer object with a given name
-        public Adventurer(string name)
+        public Adventurer(string name, Robe robe)
         {
             Name = name;
             Awesomeness = 50;
+            ColorfulRobe = robe;
+        }
+
+        public string GetDescription()
+        {
+            return $"{Name} is wearing a {ColorfulRobe} robe.";
         }
 
 
